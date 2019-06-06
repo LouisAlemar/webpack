b30a6db59d0ba700f1087a4e23871eabeab3759b
lalemar <lalemar@accessintel.com>
2019-06-06 12:50:19 -0400

    webpack mode

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ const heading = new Heading();
 heading.render();
 const helloWorldButton = new HelloWorldButton();
 helloWorldButton.render();
+
+
+if (process.env.NODE_ENV === 'production'){
+	console.log('Production Mode');
+} else if (process.env.NODE_ENV === 'development'){
+	console.log('Development Mode');
+}
+
+helloWorldButton.methodThatDoesNotExist();
